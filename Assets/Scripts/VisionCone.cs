44dbd4db31e1e57c582c79d78eeed6d940fb393b
FrankLorente <90848568+FrankLorente@users.noreply.github.com>
2024-10-10 21:38:40 +0200

    Enemigo estacionario 1
    Detecta al jugador
    Falta implementar que avise a los demas enemigos

---
+++ Assets/Scripts/VisionCone.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class VisionCone : MonoBehaviour
+{
+    public Material VisionConeMaterial;
+    public float VisionRange;
+    public float VisionAngle;
+    public LayerMask VisionObstructingLayer;
+    public int VisionConeResolution = 120;
+    Mesh VisionConeMesh;
+    MeshFilter MeshFilter_;
+    
+    void Start()
+    {
+        transform.AddComponent<MeshRenderer>().material = VisionConeMaterial;
+        MeshFilter_ = transform.AddComponent<MeshFilter>();
+        VisionConeMesh = new Mesh();
+        VisionAngle *= Mathf.Deg2Rad;
+    }
+
+    
+    void Update()
+    {
+        DrawVisionCone();
+    }
+
+    void DrawVisionCone()
+    {
+	int[] triangles = new int[(VisionConeResolution - 1) * 3];
+    	Vector3[] Vertices = new Vector3[VisionConeResolution + 1];
+        Vertices[0] = Vector3.zero;
+        float Currentangle = -VisionAngle / 2;
+        float angleIcrement = VisionAngle / (VisionConeResolution - 1);
+        float Sine;
+        float Cosine;
+
+        bool playerDetected = false; // Variable para saber si se ha detectado al jugador
+
+        for (int i = 0; i < VisionConeResolution; i++)
+        {
+            Sine = Mathf.Sin(Currentangle);
+            Cosine = Mathf.Cos(Currentangle);
+            Vector3 RaycastDirection = (transform.forward * Cosine) + (transform.right * Sine);
+            Vector3 VertForward = (Vector3.forward * Cosine) + (Vector3.right * Sine);
+            if (Physics.Raycast(transform.position, RaycastDirection, out RaycastHit hit, VisionRange, VisionObstructingLayer))
+            {
+                Vertices[i + 1] = VertForward * hit.distance;
+
+                // Verificar si el objeto golpeado es el jugador
+                if (hit.collider.CompareTag("Player"))
+                {
+                    playerDetected = true; // Se ha detectado al jugador
+                }
+            }
+            else
+            {
+                Vertices[i + 1] = VertForward * VisionRange;
+            }
+
+
+            Currentangle += angleIcrement;
+        }
+        for (int i = 0, j = 0; i < triangles.Length; i += 3, j++)
+        {
+            triangles[i] = 0;
+            triangles[i + 1] = j + 1;
+            triangles[i + 2] = j + 2;
+        }
+        VisionConeMesh.Clear();
+        VisionConeMesh.vertices = Vertices;
+        VisionConeMesh.triangles = triangles;
+        MeshFilter_.mesh = VisionConeMesh;
+
+        // Si se ha detectado al jugador, mostrar el mensaje en la consola
+        if (playerDetected)
+        {
+            Debug.Log("JUGADOR DETECTADO");
+
+        }
+    }
+
+
+}
